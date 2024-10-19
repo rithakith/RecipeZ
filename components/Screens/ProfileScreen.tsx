3db61672462cd611050d589e58528e5970bfaae3
Akith-002 <chandinubaa.22@uom.lk>
2024-10-19 22:46:42 +0530

    profile screen

--- components/Screens/ProfileScreen.tsx
+++ components/Screens/ProfileScreen.tsx
@@ -1,14 +1,50 @@
 import React, { useState, useEffect, useCallback } from "react";
-import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  ScrollView,
+} from "react-native";
 import { useNavigation, useFocusEffect } from "@react-navigation/native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Ionicons } from "@expo/vector-icons"; // Importing icons
+import RecipeCard from "../UI/RecipeCard";
 
-export default function ProfileScreen() {
+// Define a type for the recipe data
+type Recipe = {
+  recipe_id: number;
+  title: string;
+  images: string;
+  cook_time: number;
+  rating: number;
+  reviews: number;
+  description: string;
+  nutrition: {
+    carbohydrates: number;
+    protein: number;
+    calories: number;
+    fat: number;
+  };
+  ingredients: {
+    name: string;
+    unit: string;
+    quantity: number;
+    preparation?: string;
+  }[];
+  steps: string[];
+};
+
+const url = process.env.EXPO_PUBLIC_API_URL;
+
+const ProfileScreen: React.FC = () => {
   const navigation = useNavigation();
   const [user, setUser] = useState(null);
   const [userData, setUserData] = useState(null);
   const [token, setToken] = useState(null);
-  const url = process.env.EXPO_PUBLIC_API_URL;
+  const [recipes, setRecipes] = useState<Recipe[]>([]); // State to store recipes
+  const [favorites, setFavorites] = useState<Set<number>>(new Set());
 
   // Fetch data when the screen is focused
   useFocusEffect(
@@ -26,6 +62,37 @@ export default function ProfileScreen() {
     }
   }, [userData]);
 
+  useEffect(() => {
+    const fetchRecipes = async () => {
+      try {
+        const response = await fetch(`${url}/api/recipes`);
+        const data: Recipe[] = await response.json();
+        setRecipes(data);
+      } catch (error) {
+        // console.error("Error fetching recipes:", error);
+      }
+    };
+    fetchRecipes();
+  }, []);
+
+  const handleRecipeSelect = (recipe: Recipe) => {
+    // console.log(recipe);
+    navigation.navigate("Ingredients", { recipe });
+  };
+
+  // Toggle favorite status of a recipe
+  const handleFavoriteToggle = (recipeId: number) => {
+    setFavorites((prevFavorites) => {
+      const updatedFavorites = new Set(prevFavorites);
+      if (updatedFavorites.has(recipeId)) {
+        updatedFavorites.delete(recipeId);
+      } else {
+        updatedFavorites.add(recipeId);
+      }
+      return updatedFavorites;
+    });
+  };
+
   const retrieveUserData = async () => {
     try {
       const storedUserData = await AsyncStorage.getItem("userData");
@@ -33,7 +100,7 @@ export default function ProfileScreen() {
 
       if (storedUserData) {
         const parsedUserData = JSON.parse(storedUserData);
-        console.log("User Data:", parsedUserData);
+        // console.log("User Data:", parsedUserData);
         setUserData(parsedUserData);
       }
 
@@ -41,13 +108,13 @@ export default function ProfileScreen() {
         setToken(storedToken);
       }
     } catch (error) {
-      console.log("Error retrieving user data:", error);
+      // console.log("Error retrieving user data:", error);
     }
   };
 
   const checkUserExists = async (sub) => {
     try {
-      console.log("sub", sub);
+      // console.log("sub", sub);
       const response = await fetch(`${url}/api/${sub}`, {
         method: "GET",
       });
@@ -55,7 +122,7 @@ export default function ProfileScreen() {
       if (response.ok) {
         const data = await response.json(); // Parse JSON response
         setUser(data); // Set user data
-        console.log("User data from API:", data);
+        // console.log("User data from API:", data);
         return true;
       } else if (response.status === 404) {
         setUser(null);
@@ -64,106 +131,287 @@ export default function ProfileScreen() {
         throw new Error("Error checking user existence");
       }
     } catch (err) {
-      console.error("Error checking user existence:", err);
+      // console.error("Error checking user existence:", err);
       return false;
     }
   };
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Profile Information</Text>
-      <View style={styles.infoContainer}>
-        <Text style={styles.label}>Username:</Text>
-        <Text style={styles.value}>{user?.user_name || "N/A"}</Text>
-      </View>
-      <View style={styles.infoContainer}>
-        <Text style={styles.label}>Email:</Text>
-        <Text style={styles.value}>{user?.email || "N/A"}</Text>
-      </View>
-      <View style={styles.infoContainer}>
-        <Text style={styles.label}>Allergies:</Text>
-        <Text style={styles.value}>
-          {Array.isArray(user?.user_allergies)
-            ? user.user_allergies.join(", ")
-            : "None"}
-        </Text>
-      </View>
-      <View style={styles.infoContainer}>
-        <Text style={styles.label}>Dietary Preferences:</Text>
-        <Text style={styles.value}>
-          {user?.dietary_preferences?.diet_type || "None"}
-        </Text>
-      </View>
-      <View style={styles.infoContainer}>
-        <Text style={styles.label}>Health Goals:</Text>
-        <Text style={styles.value}>
-          {user?.dietary_preferences?.diet_type || "None"}
-        </Text>
-      </View>
-      <View style={styles.infoContainer}>
-        <Text style={styles.label}>Current Ingredients:</Text>
-        <Text style={styles.value}>
-          {user?.current_ingredients?.join(",") || "None"}
-        </Text>
-      </View>
-      <View style={styles.infoContainer}>
-        <Text style={styles.label}>Favorited Recipes:</Text>
-        <Text style={styles.value}>
-          {user?.favorited_recipes?.length > 0
-            ? user.favorited_recipes.join(", ")
-            : "No favorite recipes"}
-        </Text>
-      </View>
+    <ScrollView contentContainerStyle={styles.scrollContainer}>
+      <View style={styles.container}>
+        {/* Header Icons */}
+        <View style={styles.header}>
+          {/* Edit Icon */}
+          <TouchableOpacity style={styles.editButton}>
+            <Text style={{ fontSize: 16 }}>Edit</Text>
+          </TouchableOpacity>
 
-      <TouchableOpacity
-        style={styles.editButton}
-        onPress={() =>
-          navigation.navigate("QuestionScreen", { resetToFirstStep: true })
-        }
-      >
-        <Text style={styles.editButtonText}>Edit Personal Details</Text>
-      </TouchableOpacity>
-    </View>
+          {/* Title */}
+          <Text style={styles.title}>Profile Information</Text>
+
+          {/* Settings Icon */}
+          <TouchableOpacity style={styles.settingButton}>
+            <Ionicons name="settings" size={20} color="#042628" />
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.section1}>
+          {/* Profile Image */}
+          {/* <Image source={{ uri: user.image }} style={styles.profileImage} /> */}
+
+          {/* Personal Info */}
+          <Text style={styles.uname}>{user?.user_name || "N/A"}</Text>
+          <Text style={styles.email}>{user?.email || "N/A"}</Text>
+        </View>
+
+        {/*        
+        <View style={styles.infoContainer}>
+          <Text style={styles.label}>Current Ingredients:</Text>
+          <Text style={styles.value}>
+            {user?.current_ingredients?.join(",") || "None"}
+          </Text>
+        </View> */}
+
+        <View style={styles.section2}>
+          {/* Allergies */}
+          <View style={styles.section}>
+            <Text style={styles.sectionTitle}>Allergies:</Text>
+            <Text style={styles.value}>
+              {Array.isArray(user?.user_allergies)
+                ? user.user_allergies.join(", ")
+                : "None"}
+            </Text>
+          </View>
+
+          {/* Health Goals */}
+          <View style={styles.section}>
+            <Text style={styles.sectionTitle}>Health Goals:</Text>
+            <Text style={styles.value}>
+              {user?.health_goals?.diet_type || "None"}
+            </Text>
+          </View>
+
+          {/* Dietary Preferences */}
+          <View style={styles.section}>
+            <Text style={styles.sectionTitle}>Dietary Preferences:</Text>
+            <Text style={styles.value}>
+              {user?.dietary_preferences?.diet_type || "None"}
+            </Text>
+          </View>
+
+          {/* Current Ingredients */}
+          <View style={styles.section}>
+            <Text style={styles.sectionTitle}>Current Ingredients:</Text>
+            <Text style={styles.value}>
+              {user?.current_ingredients?.join(",") || "None"}
+            </Text>
+          </View>
+
+          {/* edit info button */}
+          <TouchableOpacity
+            style={styles.editInfo}
+            onPress={() => navigation.navigate("QuestionScreen")}
+          >
+            <Ionicons name="create" size={20} color="#fff" />
+            <Text style={{ color: "#fff" }}>Edit Info</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.infoContainer}>
+          <Text style={styles.label}>Favorited Recipes:</Text>
+          <Text style={styles.value}>
+            {user?.favorited_recipes?.length > 0
+              ? user.favorited_recipes.join(", ")
+              : "No favorite recipes"}
+          </Text>
+        </View>
+
+        <View style={styles.favouriteSection}>
+          <Text style={styles.favouriteTitle}>Favourite</Text>
+          <ScrollView
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            style={styles.recipeScroll}
+          >
+            {recipes.slice(0, 5).map((recipe) => (
+              <TouchableOpacity
+                key={recipe.recipe_id}
+                onPress={() => navigation.navigate("Ingredients", { recipe })}
+              >
+                <RecipeCard
+                  recipe={recipe}
+                  onFavoriteToggle={() =>
+                    handleFavoriteToggle(recipe.recipe_id)
+                  }
+                  isFavorite={favorites.has(recipe.recipe_id)}
+                  onRecipeSelect={handleRecipeSelect}
+                />
+              </TouchableOpacity>
+            ))}
+          </ScrollView>
+        </View>
+      </View>
+    </ScrollView>
   );
-}
+};
 
 const styles = StyleSheet.create({
+  scrollContainer: {
+    backgroundColor: "#fff",
+    paddingBottom: 30,
+  },
   container: {
-    flex: 1,
-    justifyContent: "center",
+    display: "flex",
     alignItems: "center",
-    padding: 20,
-    backgroundColor: "#f8f9fa",
+    paddingTop: 100,
+    paddingHorizontal: 20,
+    position: "relative",
+    height: "100%",
+    backgroundColor: "#fff",
   },
-  title: {
-    fontSize: 24,
-    fontWeight: "bold",
-    marginBottom: 20,
-  },
-  infoContainer: {
-    marginVertical: 10,
-    width: "100%",
+  header: {
     flexDirection: "row",
     justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%",
+    marginBottom: 30,
+    paddingHorizontal: 10,
+    position: "relative",
   },
-  label: {
+  title: {
+    flex: 1, // Ensures the title takes up the remaining space between the icons
+    fontSize: 22,
     fontWeight: "bold",
-    fontSize: 18,
+    textAlign: "center", // Center the text within the space
+    alignSelf: "center", // Keep the title centered within the flex space
+  },
+  editButton: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    backgroundColor: "#E8FEFF",
+    paddingHorizontal: 12,
+    paddingVertical: 7,
+    borderRadius: 12,
+    elevation: 1,
+    shadowColor: "#000",
+    position: "absolute",
+    top: -35,
+    left: 0,
+  },
+  settingButton: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    backgroundColor: "#E8FEFF",
+    paddingHorizontal: 12,
+    paddingVertical: 7,
+    borderRadius: 12,
+    elevation: 1,
+    shadowColor: "#000",
+    position: "absolute",
+    top: -35,
+    right: 0,
+  },
+  section1: {
+    width: "100%",
+    paddingTop: 50,
+    paddingHorizontal: 30,
+    paddingBottom: 20,
+    backgroundColor: "#E8FEFF",
+    borderRadius: 15,
+    marginTop: 40,
+    marginBottom: 20,
+    marginHorizontal: 20,
+    position: "relative",
+    elevation: 3,
+    shadowColor: "#000",
+  },
+  profileImage: {
+    width: 90,
+    height: 90,
+    borderRadius: 75,
+    marginBottom: 20,
+    position: "absolute",
+    left: 20,
+    top: -50,
+    borderWidth: 3,
+    borderColor: "#70b9be",
+  },
+  uname: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  email: {
+    fontSize: 16,
+    color: "#333",
+  },
+  section2: {
+    width: "100%",
+    paddingVertical: 20,
+    paddingHorizontal: 30,
+    backgroundColor: "#E8FEFF",
+    borderRadius: 15,
+    marginTop: 10,
+    marginBottom: 10,
+    marginHorizontal: 20,
+    position: "relative",
+    elevation: 3,
+    shadowColor: "#000",
+  },
+  section2: {
+    width: "100%",
+    paddingVertical: 20,
+    paddingHorizontal: 30,
+    backgroundColor: "#E8FEFF",
+    borderRadius: 15,
+    marginTop: 10,
+    marginBottom: 10,
+    marginHorizontal: 20,
+    position: "relative",
+    elevation: 3,
+    shadowColor: "#000",
   },
   value: {
     fontSize: 18,
     color: "#333",
   },
-  editButton: {
-    marginTop: 20,
+  section: {
+    marginVertical: 10,
+    width: "100%",
+  },
+  sectionTitle: {
+    fontWeight: "bold",
+    fontSize: 18,
+    marginBottom: 5,
+  },
+  editInfo: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 5,
+    backgroundColor: "#70b9be",
     paddingVertical: 10,
     paddingHorizontal: 20,
-    borderRadius: 8,
-    backgroundColor: "#70B9BE",
+    marginVertical: 10,
+    borderRadius: 15,
   },
-  editButtonText: {
-    color: "white",
-    fontSize: 16,
+  favouriteSection: {
+    width: "100%",
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  favouriteTitle: {
+    fontSize: 20,
     fontWeight: "bold",
+    marginBottom: 10,
+  },
+  recipeScroll: {
+    paddingLeft: 10,
   },
 });
+
+export default ProfileScreen;
